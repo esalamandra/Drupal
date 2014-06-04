723d4dee1fcd6c88bfdfb721536f66f01081ce55
pbasiak <pawel.basiak01@gmail.com>
2014-06-04 23:56:57 +0200

    Wstepne style

---
+++ sites/all/themes/domeniarz/templates/page.tpl.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728148
+ */
+?>
+<div id="top">
+  <header class="header" role="banner">
+
+   
+
+    <?php if ($site_name || $site_slogan): ?>
+      <div class="header__name-and-slogan">
+        <?php if ($site_name): ?>
+          <h1 class="header__site-name">
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
+          </h1>
+        <?php endif; ?>
+
+        <?php if ($site_slogan): ?>
+          <div class="header__site-slogan"><?php print $site_slogan; ?></div>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($secondary_menu): ?>
+      <nav class="header__secondary-menu" role="navigation">
+        <?php print theme('links__system_secondary_menu', array(
+          'links' => $secondary_menu,
+          'attributes' => array(
+            'class' => array('links', 'inline', 'clearfix'),
+          ),
+          'heading' => array(
+            'text' => $secondary_menu_heading,
+            'level' => 'h2',
+            'class' => array('element-invisible'),
+          ),
+        )); ?>
+      </nav>
+    <?php endif; ?>
+
+    <?php print render($page['header']); ?>
+
+  </header>
+</div>
+
+<div class="page">
+  <div class="main">
+
+    <div class="main-content" role="main">
+      <?php print render($page['highlighted']); ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="page__title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
+    </div>
+
+    <div class="main-navigation">
+      <?php print render($page['navigation']); ?>
+    </div>
+
+    <?php
+      // Render the sidebars to see if there's anything in them.
+      $sidebar_first  = render($page['sidebar_first']);
+    ?>
+
+    <?php if ($sidebar_first): ?>
+      <aside class="sidebars">
+        <?php print $sidebar_first; ?>
+      </aside>
+    <?php endif; ?>
+
+  </div>
+
+  <?php print render($page['footer']); ?>
+
+</div>
+
+<?php print render($page['bottom']); ?>
